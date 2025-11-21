63fa2d84ed5ebef3006835e2a4798ed237a5ca0d
smietanka4 <wloczewskikarol5@gmail.com>
2025-11-21 18:16:08 +0100

    zajecia 21.11.2025

---
+++ lekcja_24/Element.py
@@ -0,0 +1,45 @@
+import pygame
+
+# klasa pomocniczna Obraz
+class Obraz(pygame.sprite.Sprite):
+      def __init__(self, sciezka):
+            super().__init__()
+            self.obraz = pygame.image.load(sciezka)
+
+# klasa bazowa
+class Element():
+      def __init__(self, typ):
+            # wskaźnik wybranego elementu ubioru
+            self.wybrany = 0
+            # lista obrazów
+            self.lista_obrazow = []
+
+            # używamy pętli żeby wczytać wszystkie obrazy z folderu
+            for i in range(1,4):
+                  sciezka = f'lekcja_24/images/{typ}{i}.png'
+                  wczytany_obraz = Obraz(sciezka)
+                  self.lista_obrazow.append(wczytany_obraz)
+
+      def wybierzNastepny(self):
+            self.wybrany += 1
+            if self.wybrany > 2:
+                  self.wybrany = 0
+            
+      def wybranyObraz(self):
+            return self.lista_obrazow[self.wybrany].obraz
+      
+class NakrycieGlowy(Element):
+      def __init__(self):
+            super().__init__('head')
+
+class Stroj(Element):
+      def __init__(self):
+            super().__init__('body')
+
+class Oczy(Element):
+      def __init__(self):
+            super().__init__("eye")
+
+class Bron(Element):
+      def __init(self):
+            super().__init__("weapon")
---
+++ lekcja_24/projekt.py
@@ -0,0 +1,43 @@
+import pygame
+
+pygame.init()
+
+# Stałe
+SZEROKOSC_EKRANU = 800
+WYSOKOSC_EKRANU = 600
+
+# wczytanie obrazów do zmiennych
+obraz_tla = pygame.image.load('lekcja_24\images/background.png')
+obraz_bazy_postaci = pygame.image.load("lekcja_24\images/base.png")
+
+# utworzenie obiektów ekranu oraz FPS
+ekran = pygame.display.set_mode([SZEROKOSC_EKRANU, WYSOKOSC_EKRANU])
+zegar = pygame.time.Clock()
+
+# główna pętla gry
+gra_dziala = True
+while gra_dziala:
+      # obsługa zdarzeń
+      for zdarzenie in pygame.event.get():
+            # naciśnięcie klawiszy
+            if zdarzenie.type == pygame.KEYDOWN:
+                  if zdarzenie.key == pygame.K_ESCAPE:
+                        gra_dziala = False 
+            # naciśnięcie przycisku X
+            elif zdarzenie.type == pygame.QUIT:
+                  gra_dziala = False
+
+      # rysowanie tła
+      ekran.blit(obraz_tla, (0,0))
+      # rysowanie bazy postaci
+      ekran.blit(obraz_bazy_postaci, (270,130))
+
+      # wyczyszczenie ekranu
+      pygame.display.flip()
+      # ustalenie stałych fps na 60
+      zegar.tick(60)
+
+# koniec pętli while
+
+# zamknięcie aplikacji
+pygame.quit()
